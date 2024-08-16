09178f8e426e0f55666ccc34afe05d6f41c39a91
Nanda Kishore V <71006871+nanda1045@users.noreply.github.com>
2024-08-16 00:17:04 -0400

    data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,27 +1,34 @@
-import io
-import pandas as pd
-import requests
-if 'data_loader' not in globals():
-    from mage_ai.data_preparation.decorators import data_loader
-if 'test' not in globals():
-    from mage_ai.data_preparation.decorators import test
+from mage_ai.settings.repo import get_repo_path
+from mage_ai.io.bigquery import BigQuery
+from mage_ai.io.config import ConfigFileLoader
+from pandas import DataFrame
+from os import path
 
-@data_loader
-def load_data_from_api(*args, **kwargs):
-    """
-    Template for loading data from API
-    """
-    url = 'https://storage.googleapis.com/uber-data-engineering-project-nanda/uber_data.csv'
-    response = requests.get(url)
-    df = pd.read_csv(io.StringIO(response.text), sep=',')
-    print("Data loaded from API.")
-    print("Data shape:", df.shape)
-    print("Data columns:", df.columns)
-    return df
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
 
-@test
-def test_output(output, *args) -> None:
+@data_exporter
+def export_data_to_big_query(data, **kwargs) -> None:
     """
-    Template code for testing the output of the block.
+    Export transformed data to BigQuery.
     """
-    assert output is not None, 'The output is undefined'
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+    config_profile = 'default'
+
+    # Debugging statement to verify data keys
+    print("Data keys received for export:", data.keys())
+
+    # Export each table to BigQuery
+    for key, value in data.items():
+        table_id = 'uber-data-analytics-431702.uber_DataEngineering_yt.{}'.format(key)
+        BigQuery.with_config(ConfigFileLoader(config_path, config_profile)).export(
+            DataFrame(value),
+            table_id,
+            if_exists='replace',  # Specify resolution policy if table name already exists
+        )
+
+    
+
+    
+
+
